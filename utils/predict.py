8d5b82a9dbe6bfd25f85628c785ad8a2cae70df5
MaheshRKumawat <maheshkumawat0911@gmail.com>
2022-11-15 01:22:15 +0530

    roberta predict

--- utils/predict.py
+++ utils/predict.py
@@ -5,6 +5,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from Resources.Speech.utils import *
+from Resources.Text.utils import *
 
 labels = ['Angry', 'Fear', 'Happy', 'Neutral', 'Sad']
 
@@ -20,4 +21,15 @@ def predict_speech(raw_speech):
     y_pred_test = speech_model.predict(test_final)
     for emo in y_pred_test:
         emotion = labels[np.argmax(emo)]
+    return emotion
+
+
+def predict_text(text_sentence, max_len):
+    preprocessed_text = preprocess(text_sentence)
+    input_ids, attention_masks = roberta_inference_encode(
+        preprocessed_text, maximum_length=max_len)
+    roberta_text_model = load_model(
+        'Emotion-Recognition-using-Text-with-Emojis-and-Speech/model/roberta.h5', compile=False)
+    result = roberta_text_model.predict([input_ids, attention_masks])
+    emotion = labels[np.argmax(result)]
     return emotion
