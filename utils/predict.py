e9447cc72cdf99dc465a2d3eece433e7a6fc126d
MaheshRKumawat <maheshkumawat0911@gmail.com>
2022-11-17 01:07:51 +0530

    Made few changes

--- utils/predict.py
+++ utils/predict.py
@@ -19,6 +19,19 @@ from tensorflow.keras.models import load_model
 
 labels = ['Angry', 'Fear', 'Happy', 'Neutral', 'Sad']
 
+# def predict_speech(raw_speech):
+#     speech_model = load_model(
+#         "Emotion-Recognition-using-Text-with-Emojis-and-Speech/model/speech_final_model.h5", compile=False)
+#     test_audio_arrays = []
+#     test_x, _ = librosa.load(raw_speech, sr=44100)
+#     test_audio_arrays.append(test_x)
+#     test_feature = get_features(np.array(test_x))
+#     test_final = np.expand_dims(test_feature, axis=2)
+#     y_pred_test = speech_model.predict(test_final)
+#     for emo in y_pred_test:
+#         emotion = labels[np.argmax(emo)]
+#     return emotion
+
 
 def predict_speech(raw_speech):
     '''
